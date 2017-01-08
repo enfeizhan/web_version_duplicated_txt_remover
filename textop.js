c129368b173286c1f934294ccbf6eff1d8332801
Fei Zhan <enfeizhan@gmail.com>
2017-01-08 21:01:52 +1100

    use vanilla js

--- textop.js
+++ textop.js
@@ -1,61 +1,77 @@
-$(document).ready(function(){
-  $("#submit").click(function(){
-    var textContent = $("#textinput").val();
+function remove()
+{
+    var textContent = document.getElementById("textinput").value;
     var previousWord = "";
     var presentWord = "";
     var textOutput = "";
     for (var i = 0; i < textContent.length; i++)
     {
-      if (textContent[i] == " ")
-      {
-	//detect a new word in text
-	if ( presentWord != previousWord)
-	{
-	  // non-double-typing
-	  textOutput = textOutput + presentWord + " ";
-	  previousWord = presentWord;
-	  presentWord = "";
-	}else
-	{
-	  // double-typing
-	  var removeConfirm = confirm("Remove the double-typed word \"" + presentWord +"\" in \"" + previousWord + " " + presentWord + "\"?");
-	  if (removeConfirm == true)
-	  {
-	    presentWord = "";
-	    // yes I wanna remove the duplicated text
-	  } else {
-	    // no 
-	    textOutput = textOutput + presentWord + " ";
-	    previousWord = presentWord;
-	    presentWord = "";
-	  }	  
-	}
-      }else if (textContent[i] == "\n")
-      {
-	// newline character also detects a new word
-	if ( presentWord != previousWord)
-	{
-	  // non-double-typing
-	  textOutput = textOutput + presentWord + "\n";
-	  previousWord = presentWord;
-	  presentWord = "";
-	}else
-	{
-	  // double-typing
-	  textOutput = textOutput + "\n";
-	  presentWord = "";
-	}	
-      }else
-      {
-	// no new word detected, keep receiving new characters to form a word
-	presentWord = presentWord + textContent[i];
-      }
+        if (textContent[i] == " ")
+        {
+	        //detect a new word in text
+	        if (presentWord != previousWord)
+	        {
+	            // non-double-typing
+	            textOutput = textOutput + presentWord + " ";
+	            previousWord = presentWord;
+	            presentWord = "";
+	        }
+            else
+            {
+	            // double-typing
+	            var removeConfirm = confirm(
+                    "Remove the double-typed word \""
+                    + presentWord 
+                    +"\" in \"" 
+                    + previousWord 
+                    + " " 
+                    + presentWord 
+                    + "\"?"
+                );
+	            if (removeConfirm == true)
+	            {
+	                presentWord = "";
+	                // yes I wanna remove the duplicated text
+	            }
+                else
+                {
+	                // no 
+	                textOutput = textOutput + presentWord + " ";
+	                previousWord = presentWord;
+	                presentWord = "";
+	            }	  
+	        }
+              
+        }
+        else if (textContent[i] == "\n")
+        {
+	        // newline character also detects a new word
+	        if (presentWord != previousWord)
+	        {
+	            // non-double-typing
+	            textOutput = textOutput + presentWord + "\n";
+	            previousWord = presentWord;
+	            presentWord = "";
+	        }
+            else
+	        {
+	            // double-typing
+	            textOutput = textOutput + "\n";
+	            presentWord = "";
+	        }	
+        }
+        else
+        {
+	        // no new word detected,
+            // keep receiving new characters to form a word
+	        presentWord = presentWord + textContent[i];
+        }
     }
     if ( presentWord != previousWord)
     {
-      textOutput = textOutput + presentWord;
-    // add the last word
+        // add the last word
+        textOutput = textOutput + presentWord;
     }
-    $("#textoutput").text(textOutput);
-  });
-}); 
+    console.log(textOutput)
+    document.getElementById("textoutput").innerHTML = textOutput;
+}
