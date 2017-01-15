a0af4d267d64311b67db4c97ecd737b985b84a69
Fei Zhan <enfeizhan@gmail.com>
2017-01-15 21:28:28 +1100

    refator using javascripts functions

--- textop.js
+++ textop.js
@@ -1,76 +1,36 @@
-function remove()
-{
+function remove() {
     var textContent = document.getElementById("textinput").value;
     var previousWord = "";
     var presentWord = "";
     var textOutput = "";
-    for (var i = 0; i < textContent.length; i++)
-    {
-        if (textContent[i] == " ")
-        {
-	        //detect a new word in text
-	        if (presentWord != previousWord)
-	        {
-	            // non-double-typing
-	            textOutput = textOutput + presentWord + " ";
-	            previousWord = presentWord;
-	            presentWord = "";
-	        }
-            else
-            {
-	            // double-typing
-	            var removeConfirm = confirm(
-                    "Remove the double-typed word \""
-                    + presentWord 
-                    +"\" in \"" 
-                    + previousWord 
-                    + " " 
-                    + presentWord 
-                    + "\"?"
-                );
-	            if (removeConfirm == true)
-	            {
-	                presentWord = "";
-	                // yes I wanna remove the duplicated text
-	            }
-                else
-                {
-	                // no 
-	                textOutput = textOutput + presentWord + " ";
-	                previousWord = presentWord;
-	                presentWord = "";
-	            }	  
-	        }
-              
+    var words = textContent.split(" ")
+    var res = [];
+    var cleanedWord = "";
+    var wordsLength = words.length;
+    for (i = 0; i < wordsLength; i++){
+        presentWord = words[i];
+        cleanedWord = presentWord.replace("\n", "");
+        if (cleanedWord == previousWord) {
+	        // double-typing
+	        var removeConfirm = confirm(
+                "Remove the double-typed word \""
+                + presentWord 
+                +"\" in \"" 
+                + previousWord 
+                + " " 
+                + presentWord 
+                + "\"?"
+            );
+	        if (removeConfirm == true){ }
+            else {
+                res.push(presentWord);
+	        }	  
+        } else{
+            res.push(presentWord);
+            previousWord = presentWord;
         }
-        else if (textContent[i] == "\n")
-        {
-	        // newline character also detects a new word
-	        if (presentWord != previousWord)
-	        {
-	            // non-double-typing
-	            textOutput = textOutput + presentWord + "\n";
-	            previousWord = presentWord;
-	            presentWord = "";
-	        }
-            else
-	        {
-	            // double-typing
-	            textOutput = textOutput + "\n";
-	            presentWord = "";
-	        }	
-        }
-        else
-        {
-	        // no new word detected,
-            // keep receiving new characters to form a word
-	        presentWord = presentWord + textContent[i];
-        }
+
     }
-    if ( presentWord != previousWord)
-    {
-        // add the last word
-        textOutput = textOutput + presentWord;
-    }
+    textOutput = res.join(" ");
     document.getElementById("textoutput").innerHTML = textOutput;
 }
